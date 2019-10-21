d051e5a3ba2655b4c533afff048f6c854ba9ed72
memek <memek@onet.pl>
2019-10-21 08:27:21 +0200

    Change to react component class.

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-function Contact({contact}) {
+class Contact extends Component {
 
-    const {name,email,phone} = contact;
-    return (
-        <div className="card card-body mb-3">
-            <h4 style={{color: 'red'}}>{name}</h4>
-            <ul className="list-group">
-                <li className="list-group-item">{email}</li>
-                <li className="list-group-item">{phone}</li>
-            </ul>
-        </div>
-    );
+    state={};
+
+    onShowContactClick = (e) => {
+        console.log(e.target);
+    };
+
+    render() {
+
+        let {contact} = this.props;
+        const {name, email, phone} = contact;
+
+        return (
+            <div className="card card-body mb-3">
+                <h4 style={{color: 'red'}}>{name}
+                    <i onClick={this.onShowContactClick} className="fas fa-sort-down"/>
+                </h4>
+                <ul className="list-group">
+                    <li className="list-group-item">{email}</li>
+                    <li className="list-group-item">{phone}</li>
+                </ul>
+            </div>
+        );
+    }
 }
 
 Contact.propTypes = {
