fea31472d35ce28e38af5ea64245cbd76519b498
memek <memek@onet.pl>
2019-10-21 12:19:41 +0200

    Back to class component + delete functionality added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,31 +1,39 @@
-import React,{useState}  from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-const Contact = ({contact}) => {
-    const {name, email, phone} = contact;
-    const [showContactInfo, setShowContactInfo] = useState(false);
-    const handleClick = () => setShowContactInfo(!showContactInfo);
+class Contact extends Component {
+    state = {showContactInfo: false};
+    render() {
+        let {contact} = this.props;
+        const {name, email, phone} = contact;
+        const handleShowInfoClick = () => this.setState({showContactInfo: !this.state.showContactInfo});
 
-    return (
-        <div className="card card-body mb-3">
-            <h4 style={{color: 'red'}}>{name}{' '}
-                <i onClick={handleClick
-                } className="fas fa-sort-down"/>
-            </h4>
-            {showContactInfo ? (
-                <ul className="list-group">
-                    <li className="list-group-item">{email}</li>
-                    <li className="list-group-item">{phone}</li>
-                </ul>) : null}
-        </div>
-    );
+        const handleDeleteClick = () => {
+            this.props.deleteClickHandler();
+        };
+
+        return (
+            <div className="card card-body mb-3">
+                <h4>{name}{' '}
+                    <i onClick={handleShowInfoClick} className="fas fa-sort-down" style={{cursor: 'pointer'}}/>
+                    <i onClick={handleDeleteClick} className="fas fa-times" style={{cursor: 'pointer', float: 'right'}}/>
+
+                </h4>
+                {this.state.showContactInfo ? (
+                    <ul className="list-group">
+                        <li className="list-group-item">{email}</li>
+                        <li className="list-group-item">{phone}</li>
+                    </ul>) : null}
+            </div>
+        );
 
 
-
-};
+    }
+}
 
 Contact.propTypes = {
     contact: PropTypes.object.isRequired,
+    deleteClickHandler: PropTypes.func.isRequired
 };
 
 export default Contact;
--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -32,10 +32,16 @@ class Contacts extends Component {
         ]
     };
 
+    deleteContact = (id) =>{
+        const {contacts} = this.state;
+        const newContacts = contacts.filter((contact)=> contact.id!==id);
+        this.setState({contacts: newContacts});
+    };
+
     render() {
         const {contacts} = this.state;
         return (<React.Fragment>
-                {contacts.map(contact => <Contact key={contact.id} contact={contact}/>)}
+                {contacts.map(contact => <Contact key={contact.id} contact={contact} deleteClickHandler={this.deleteContact.bind(this,contact.id)}/>)}
             </React.Fragment>
         );
     };
