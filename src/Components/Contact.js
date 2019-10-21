0b5c1257200373d7ef2a22cb71ebb8c3a4927acd
memek <memek@onet.pl>
2019-10-21 10:34:14 +0200

    Move to react functional component

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,33 +1,28 @@
-import React, {Component} from 'react';
+import React,{useState}  from 'react';
 import PropTypes from 'prop-types';
 
-class Contact extends Component {
+const Contact = ({contact}) => {
+    const {name, email, phone} = contact;
+    const [showContactInfo, setShowContactInfo] = useState(false);
+    const handleClick = () => setShowContactInfo(!showContactInfo);
 
-
-    state = {
-        showContactInfo: false
-    };
-
-    render() {
-
-        const {name, email, phone} = this.props.contact;
-        const {showContactInfo} = this.state;
-        return (
-            <div className="card card-body mb-3">
-                <h4 style={{color: 'red'}}>{name}{' '}
-                    <i onClick={() =>
-                        this.setState({showContactInfo : !this.state.showContactInfo})
-                    } className="fas fa-sort-down"/>
-                </h4>
-                {showContactInfo ? (
-                <ul className="list-group" >
+    return (
+        <div className="card card-body mb-3">
+            <h4 style={{color: 'red'}}>{name}{' '}
+                <i onClick={handleClick
+                } className="fas fa-sort-down"/>
+            </h4>
+            {showContactInfo ? (
+                <ul className="list-group">
                     <li className="list-group-item">{email}</li>
                     <li className="list-group-item">{phone}</li>
-                </ul>): null}
-            </div>
-        );
-    }
-}
+                </ul>) : null}
+        </div>
+    );
+
+
+
+};
 
 Contact.propTypes = {
     contact: PropTypes.object.isRequired,
